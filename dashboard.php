086eb8cbf2f15e472db22e18327e723f8d64556d
grygore7 <98840339+grygore7@users.noreply.github.com>
2026-05-05 22:03:49 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -65,8 +65,8 @@ $richieste_ricevute_passate= array_filter($richieste_ricevute_raw, fn($r) => str
     <title>Dashboard - OnePassage</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/design-system.css">
-    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="design-system.css">
+    <link rel="stylesheet" href="dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
