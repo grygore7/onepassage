4b7396419760a5a14832451193b105167c5597e0
grygore7 <98840339+grygore7@users.noreply.github.com>
2026-05-23 22:59:21 +0200

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ $password = getenv('MYSQLPASSWORD') ?: '';
 try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
-        $username, $password,
+        $user, $password,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
          PDO::ATTR_EMULATE_PREPARES   => false]
